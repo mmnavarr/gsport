f46f113d2e5c9ed8bf566f5a8b8dc2e1bef30883
mmnavarr <mmnavarr@live.com>
2025-03-03 17:40:19 -0600

    Using local CSS file instead of import

--- ui-kit/src/main.tsx
+++ ui-kit/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./css/index.css";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(<App />);
